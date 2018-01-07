0762da71826d3844c3a931f37303a544f24935fa
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-07 16:47:10 -0500

    set up pets controller

--- FindAPet2/app/controllers/pets_controller.rb
+++ FindAPet2/app/controllers/pets_controller.rb
@@ -1,2 +1,53 @@
-class PetsController < ApplicationController
+class PetsController < ApiController
+	before_action :require_login, except: [:index, :show]
+
+	def index
+		@pets = Pet.all.order(id: :desc)
+	end
+
+	def show
+		@pet = Pet.find(params[:id])
+		@pets = Pet.all.order(id: :desc)
+	end
+
+	def new
+		@pet = Pet.new		
+	end
+
+	def create
+		@pet = Pet.new(pet_params)
+		@pet.user = current_user
+		if @pet.save
+			redirect_to pet_path(@pet)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@pet = Pet.find(params[:id])
+	end
+
+	def update
+		@pet = Pet.find(params[:id])
+		if @pet.update(pet_params)
+			redirect_to pet_path(@pet)
+		else
+			redirect_to pet_edit_path(@pet)
+		end		
+	end
+
+	def destroy
+		@pet = Pet.find(params[:id])
+		@pet.delete
+		redirect_to pets_path
+	end
+
+	private
+	def pet_params
+		params.require(:pet).permit(:name, :post_type, :animal, :breed, :age, :picture, :picture_content_type, :picture_file_name, :picture_file_size, :picture_updated_at, :description)
+	end
+
 end
+
+
