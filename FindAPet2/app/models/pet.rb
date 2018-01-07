a28bc487b076ebf76145ae5091094bbce0fcea3e
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-07 14:16:59 -0500

    setup user and pet models

--- FindAPet2/app/models/pet.rb
+++ FindAPet2/app/models/pet.rb
@@ -1,2 +1,11 @@
 class Pet < ApplicationRecord
+	#set required fields
+	validates :name, :post_type, :aniaml, :presence => true
+	#declares model has an attachment in the picture column
+	has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
+	#validates the content type being uploaded is an image
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+	belongs_to :user
 end
+
+# validates :name, :muscle_group, :sets, :reps, :date, :presence => true
--- FindAPet2/app/models/user.rb
+++ FindAPet2/app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+	#makes sure username is only repeated once in database
+	validates_uniqueness_of :username
+	#bcrypt method that handles password hashing
+  has_secure_password
+  has_secure_token :auth_token
+  has_many :pets
+
+  #used to logout
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  #makes sure use of built-in auth method bcrypt gives and hashes the password against the password_digest in the db
+  def self.validate_login(username, password)
+    user = find_by(username: username)
+    if user && user.authenticate(password)
+      user
+    end
+  end
 end
+
+
