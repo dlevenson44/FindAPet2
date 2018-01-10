45950af62a643596f483a9afaa55dea0394bdf09
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-09 22:25:51 -0500

    setup index view, login view

---
+++ FindAPet2/app/controllers/session_controller.rb
@@ -0,0 +1,37 @@
+class SessionController < ApplicationController
+
+	before_action :ensure_signed_out, only: [:new, :create]
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		username = user_params[:username]
+		password = user_params[:password]
+
+		user = User.find_from_credentials(username, password)
+
+		if user
+			sign_in(user)
+			flash[:notice] = "#{username} has successfully signed in."
+			redirect_to "/"
+		else
+			flash[:error] = "User not found."
+			@user = User.new(username: username)
+			render :new
+		end
+	end
+
+	def destroy
+		sign_out
+		flash[:notice] = "User signed out."
+		redirect_to "/"
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
+end
