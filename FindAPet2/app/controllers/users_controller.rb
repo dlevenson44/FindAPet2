f30a8716c3a1e4f840fa0b5d6ed39b4ba904e008
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-10 17:31:29 -0500

    log out button rendering without error

--- FindAPet2/app/controllers/users_controller.rb
+++ FindAPet2/app/controllers/users_controller.rb
@@ -1,36 +1,36 @@
 class UsersController < ApplicationController
 
-	before_action :ensure_signed_out, only: [:new, :create]
-	before_action :ensure_signed_in, only: [:show, :index]
+  before_action :ensure_signed_out, only: [:new, :create]
+  before_action :ensure_signed_in, only: [:show, :index]
 
-	def index
-		@user = User.all
-	end
+  def new
+    @user = User.new
+  end
 
-	def new
-		@user = User.new
-	end
+  def create
+    @user = User.new(create_params)
 
-	def create
-		@user = User.new(create_params)
+    if @user.save
+      sign_in(@user)
+      flash[:notice] = 'You are signed in!'
+      redirect_to users_path
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
 
-		if @user.save
-			sign_in(@user)
-			flash[:notice] = 'You have successfully signed in.'
-			redirect_to "/"
-		else
-			flash[:error] = @user.errors.full_messages.join(', ')
-			render :new
-		end
-	end
+  def index
+    @users = User.all
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	private
+  private
 
-	def create_params
-		params.require(:user).permit(:username, :password, :f_name, :l_name, :address, :city, :state, :zip, :phone, :email)
-	end
+  def create_params
+    params.require(:user).permit(:username, :password, :f_name, :l_name, :address, :city, :state, :zip, :phone, :email)
+  end
 end
--- FindAPet2/config/routes.rb
+++ FindAPet2/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "root#index"
- 	resources :users, only: [:new, :create, :index, :show]
- 	resources :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :index, :show]
+  resource :session, only: [:new, :create, :destroy]
   resources :pets
 end
 
