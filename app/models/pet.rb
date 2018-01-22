c604552561112c86f0855c2d9beda2da9f82480d
Daniel Levenson <daniellevenson4@gmail.com>
2018-01-22 15:51:57 -0500

    changed image size in model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ApplicationRecord
 	#declares model has an attachment in the picture column
-	has_attached_file :picture, styles: { large: "600x600>", medium: "300x300>" }
+	has_attached_file :picture, styles: { large: "600x600>", medium: "450x450>" }
 	#validates the content type being uploaded is an image
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
