875489e0f147e5f5d92670b28f4aed1c54260348
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-12 22:17:58 -0500

    added testing for username/email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 	# initiate new user
 	let(:user) {User.new}
+	let(:user1) {User.new}
 
 	# test new account without Username, expacted to be invalid
 	it "is not valid without a username" do
@@ -20,6 +21,13 @@ RSpec.describe User, type: :model do
 		user.username = 'a' * 26
 		expect(user).not_to be_valid
 	end
+
+	# test duplicate username
+	it "cannot use the same username multiple times" do
+		user.username = 'a'
+		user1.username = 'a'
+		expect(user1).not_to be_valid
+	end
 	
 	# test password min length
 	it "must have at least 6 chars in password" do
@@ -31,5 +39,12 @@ RSpec.describe User, type: :model do
 	it "must have no more than 25 chars in password" do
 		user.password = 'a' * 26
 		expect(user).not_to be_valid
+	end
+
+	# test multiple email addresses
+	it "cannot use the same email multiple times" do
+		user.email = 'dan@dan.com'
+		user1.email = 'dan@dan.com'
+		expect(user1).not_to be_valid
 	end
 end
