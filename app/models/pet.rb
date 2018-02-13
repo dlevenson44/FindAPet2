b0fe8f6e221a5cef7aaea3291794c55b33b1b1c1
Daniel Levenson <daniellevenson4@gmail.com>
2018-02-12 22:23:25 -0500

    added field requirements to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,9 @@ class Pet < ApplicationRecord
 	#declares model has an attachment in the picture column
 	has_attached_file :picture, styles: { large: "600x600>", medium: "450x450>" }
 	#validates the content type being uploaded is an image
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+  	validates :name, :post_type, :animal, :breed, :age, :description, :presence => true
 
 	belongs_to :user
 end
